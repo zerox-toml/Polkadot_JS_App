b4cd1f9089e4c0393acde574ef31e65f2051c904
zerox-toml <passionkaizen96111@gmail.com>
2025-06-12 16:25:33 +0200

    feat:update quantova header

--- packages/apps-config/src/api/spec/quantova.ts
+++ packages/apps-config/src/api/spec/quantova.ts
@@ -1,3 +1,6 @@
+// Copyright 2017-2025 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
 import { quantovaDefinitions } from '@quantova-network/types-bundle';
 
 export default quantovaDefinitions;
